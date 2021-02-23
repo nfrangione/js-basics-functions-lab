e5a80625dd633c8f6ac98fb83218be1f0dbf1fba
Nicholas V Frangione <nfrangione8@gmail.com>
2021-02-23 09:39:54 -0800

    commit tree 714cddbda41d57fe8ffb9cbb8105180728220cac

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+const hqLocation = 42;
+
+function distanceFromHqInBlocks(location){
+    let distanceFromHQBlocks = Math.abs(location - hqLocation);
+    return distanceFromHQBlocks;
+}
+
+function distanceFromHqInFeet(location) {
+    let distanceFromHQFeet = Math.abs(location - hqLocation) * 264;
+    return distanceFromHQFeet;
+}
+
+function distanceTravelledInFeet(start, end) {
+    let distanceTravelled = Math.abs(start - end) * 264;
+    return distanceTravelled;
+}
+
+function calculatesFarePrice(start, destination) {
+    let farePrice = 0;
+    let blocks = Math.abs(start - destination);
+    let feet = blocks * 264;
+    if (feet <= 400){
+        return farePrice;
+    }
+    else if (feet > 400 && feet <= 2000) {
+        farePrice = (feet-400) * 0.02;
+        return farePrice
+    }
+    else if (feet > 2000 && feet < 2500){
+        farePrice = 25;
+        return farePrice;
+    }
+    else {
+        return 'cannot travel that far'
+    }
+}
